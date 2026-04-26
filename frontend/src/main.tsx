81e8fa13692ee86521d4d83d54e27a69e28a220d
saulandresv <saul.vega2@gmail.com>
2026-04-26 12:56:57 -0400

    feat: set MapCardView as default view

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
 import { MapCardView } from './views/MapCardView'
 import './App.css'
 
@@ -12,12 +11,10 @@ const queryClient = new QueryClient({
   },
 })
 
-const isTest = window.location.hash === '#/test'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      {isTest ? <MapCardView /> : <App />}
+      <MapCardView />
     </QueryClientProvider>
   </React.StrictMode>
 )
